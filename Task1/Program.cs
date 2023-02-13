2af0f7336e24d3ca762c6146c258025722497e9c
Maxim Kornilov <M.kornilov.flights@gmail.com>
2023-02-13 11:13:04 +0300

    задача на сумму чисел от 1 до A

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// // Напишите программу, которая принимает на 
+// вход число (А) и выдает сумму чисел
+// от 1 до А.
+
+// 7 -> 28
+// 4 -> 10
+
+int num1 = 7;
+System.Console.WriteLine(GetSum(num1));
+
+static int GetSum(int num)
+{
+    int res = 0;
+ for(int i =0; i <= num; i++)
+ {
+    res += i;
+ } 
+ return res;  
+}
