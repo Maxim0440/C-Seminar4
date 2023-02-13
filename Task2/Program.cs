6fbb56300d99cbde175db1189f9b00a7b8e8e167
Maxim Kornilov <M.kornilov.flights@gmail.com>
2023-02-13 12:05:06 +0300

    задача 26. нахождение количества цифр в произвольном числе

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 26: Напишите программу, которая 
+// принимает на вход число и 
+// выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+System.Console.WriteLine(GetCountNum(new Random().Next(1000, 100000)));
+
+static int GetCountNum(int num)
+{
+    System.Console.WriteLine(num);
+    int count = 0;
+    while(num > 0)
+    {
+        count++;
+        num /= 10;
+    }
+    return count;
+}
